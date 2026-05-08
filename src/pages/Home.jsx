c1424cbd73f421da798f54ac99730b0e8e16cb54
JorshDuck <jrvgsh@gmail.com>
2026-05-07 23:30:43 -0400

    Hero and Navbar fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import '../Assets/Styles/Home.css'
+
 function Home() {
   return (
-    <div>
-      <h1>Página de Inicio</h1>
-      <p>Aquí irá la Hero Section</p>
+    <div className='home'>
+      <section className='hero'> 
+        <div className='hero-overlay'></div>
+        <div className='hero-content'>
+          <h1>Vitality & Glow</h1>
+          <p>Terapías profesionales diseñadas para reconciliar tu cuerpo con tu mente.</p>
+          <a href="/booking" className='hero-btn'>Reserva tu sesión</a>
+        </div>
+      </section>
     </div>
   )
 }
