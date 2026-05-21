9a75412659356b03710d00abed89759cbc01527c
JorshDuck <jrvgsh@gmail.com>
2026-05-21 16:30:43 -0400

    Data Fixed and Project Finished

--- src/Data/Services.js
+++ src/Data/Services.js
@@ -6,7 +6,7 @@ const servicios = [
     duration: "1 Hora",
     price: "50 Bs.",
     image: "LimpiezaFacialBasica.jpg",
-    description: "Descripción de Limpieza Facial Básica"
+    description: "Higiene profunda del rostro que elimina impurezas. Ideal para mantener la piel fresca, hidratada y libre de imperfecciones."
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const servicios = [
     duration: "1 Hora",
     price: "70 Bs.",
     image: "limpiezaFacialProfunda.jpg",
-    description: "Descripción de Limpieza Facial Profunda"
+    description: "Tratamiento facial completo con exfoliación y vapor, para extraer puntos negros y eliminar celulas muertas. Combate el acné y cierra poros."
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const servicios = [
     duration: "1 Hora",
     price: "70 Bs.",
     image: "limpiezaProfundaEspalda.jpg",
-    description: "Descripción de Limpieza Profunda de Espalda"
+    description: "Exfoliación y extracción de impurezas y celulas muertas en la espalda. Perfecta para eliminar acné, mejorar la textura de la piel, lucir una espalda suave y limpia."
   },
   {
     id: 4,
@@ -33,37 +33,34 @@ const servicios = [
     duration: "45 min",
     price: "50 Bs.",
     image: "MasajeRelajante.jpg",
-    description: "Descripción de Masaje Relajante"
+    description: "Técnica suave de movimientos intensos que alivia tensiones y reduce el estrés. Mejora el sueño y relaja los músculos."
   },
   {
-    // Falta
     id: 5 ,
     name: "Masaje Descontracturante",
     tag: "masaje",
     duration: "30 min",
-    price: "30 Bs.",
+    price: "50 Bs.",
     image: "MasajeDescontracturante.jpg",
-    description: "Descripción de Masaje Descontracturante"
+    description: "Técnica de masaje de presión firme sobre nudos y contracturas musculares. Alivia dolores cervicales y de espalda por malas posturas o sobrecarga física."
   },
   {
-    // Falta
     id: 6,
     name: "Drenaje Linfático",
     tag: "masaje",
     duration: "30 min",
-    price: "30 Bs.",
+    price: "60 Bs.",
     image: "DrenajeLinfatico.jpg",
-    description: "Descripción de Drenaje Linfático"
+    description: "Técnica de masaje suave que activa el sistema linfático. Reduce la retención de líquidos, combate la hinchazón, refuerza el sistema inmunológico y ayuda a eliminar toxinas."
   },
   {
-    // Falta
     id: 7,
     name: "Drenaje Brasileño",
     tag: "masaje",
     duration: "30 min",
-    price: "30 Bs.",
+    price: "80 Bs.",
     image: "DrenajeBrasileño.jpg",
-    description: "Descripción de Drenaje Brasileño"
+    description: "Técnica de masaje con movimientos intensos. Moldea la figura y disminuye la retencion de toxinas en las células grasas para un abdomen más plano."
   },
   {
     id: 8,
@@ -72,76 +69,70 @@ const servicios = [
     duration: "1 Hora",
     price: "70 Bs.",
     image: "Maderoterapia.jpg",
-    description: "Descripción de Maderoterapia"
+    description: "Tratamiento con herramientas de madera que activa la circulación. Reduce grasa localizada, tonifica la piel y esculpe la figura del cuerpo definiendo la cintura y piernas."
   },
   {
-    // Falta
     id: 9,
     name: "Baño de Luna",
     tag: "tratamiento",
     duration: "30 min",
-    price: "30 Bs.",
+    price: "60 Bs.",
     image: "BañoLuna.jpg",
-    description: "Descripción de Baño de Luna"
+    description: "Tratamiento estético y relajante. Hidrata y exfolia el vello corporal dejando una piel luminosa y suave."
   },
   {
     id: 10,
     name: "Chocoterapia",
     tag: "tratamiento",
-    duration: "30 min",// Falta
+    duration: "45 min",
     price: "60 Bs.",
     image: "Chocoterapia.jpg",
-    description: "Descripción de Chocoterapia"
+    description: "Tratamiento corporal con cacao puro rico en antioxidantes. Nutre la piel, estimula la producción de colágeno y proporciona un placer sensorial relajante para calmar la ansiedad."
   },
   {
-    // Falta
     id: 11,
     name: "Cafeterapia",
     tag: "tratamiento",
-    duration: "30 min",
-    price: "30 Bs.",
+    duration: "45 min",
+    price: "80 Bs.",
     image: "Cafeterapia.jpg",
-    description: "Descripción de Cafeterapia"
+    description: "Tratamiento corporal con café molido. Exfolia el cuerpo mejorando la circulación. Reduce la celulitis, elimina células muertas y deja la piel más firme."
   },
   {
-    // Falta
     id: 12,
     name: "Fangoterapia",
     tag: "tratamiento",
-    duration: "30 min",
-    price: "30 Bs.",
+    duration: "45 min",
+    price: "60 Bs.",
     image: "Fangoterapia.jpg",
-    description: "Descripción de Fangoterapia"
+    description: "Tratamiento corporal de barro rico en minerales que desintoxica la piel y reduce la inflamación. Ideal para aliviar dolores y absorver impurezas."
   },
   {
-    // Falta
     id: 13,
     name: "Vendas Frías",
     tag: "tratamiento",
-    duration: "30 min",
-    price: "30 Bs.",
+    duration: "45 min",
+    price: "120 Bs.",
     image: "VendasFrias.jpg",
-    description: "Descripción de Vendas Frías"
+    description: "Tratamiento estético con vendas impregnadas en lociones. Provocando al cuerpo a quemar grasa localizada y disminuir flacidez."
   },
   {
-    // Falta
     id: 14,
     name: "Masaje Capilar con Botox",
     tag: "tratamiento",
-    duration: "30 min",
-    price: "30 Bs.",
+    duration: "1 Hora",
+    price: "80 Bs.",
     image: "MasajeCapilarBotox.jpg",
-    description: "Descripción de Masaje Capilar con Botox"
+    description: "Tratamiento intensivo que repara y reconstruye el cabello restaurando cabellos dañados por tintes y decoloraciones. Hidrata y nutre el cabello aportando brillo y suavidad."
   },
   {
-    // Falta
     id: 15,
     name: "Depilación con Cera",
     tag: "depilacion",
     duration: "30 min",
-    price: "30 Bs.",
+    price: "60 Bs.",
     image: "DepilacionCera.jpg",
-    description: "Descripción de Depilación con cera"
+    description: "Eliminación del vello desde la raíz con cera de calidad. Para una piel suave y libre de vello por más tiempo. Rápida, efectiva y con acabado profesional."
   },
 ]
 
