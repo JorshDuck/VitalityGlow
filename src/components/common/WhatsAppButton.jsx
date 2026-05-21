02a44f49b1c63add002ca9f4a572ed35d08c4ba1
JorshDuck <jrgvsh@gmail.com>
2026-05-21 18:15:35 -0400

    Fix: WAB Config path

--- src/components/common/WhatsAppButton.jsx
+++ src/components/common/WhatsAppButton.jsx
@@ -1,5 +1,5 @@
 import { SiWhatsapp } from 'react-icons/si'
-import { URLS, MESSAGES } from '../../data/config'
+import { URLS, MESSAGES } from '../../data/Config'
 import '../../assets/styles/WhatsAppButton.css'
 
 function WhatsAppButton() {
