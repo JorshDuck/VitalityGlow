f24fa6b3452f78a0980c0d4c2f5851368aaa20fe
JorshDuck <jrgvsh@gmail.com>
2026-05-21 18:06:59 -0400

    Recreate: WhatsAppButton

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/common/Navbar'
 import Footer from './components/common/Footer'
+import WhatsAppButton from './components/common/WhatsAppButton'
 import Home from './pages/Home'
 import Services from './pages/Services'
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
+      <WhatsAppButton />
     </BrowserRouter>
   )
 }
---
+++ src/components/common/WhatsAppButton.jsx
@@ -0,0 +1,18 @@
+import { SiWhatsapp } from 'react-icons/si'
+import { URLS, MESSAGES } from '../../data/config'
+import '../../assets/styles/WhatsAppButton.css'
+
+function WhatsAppButton() {
+  return (
+    <a
+      className='whatsapp-btn'
+      href={URLS.whatsapp(MESSAGES.reservation)}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <SiWhatsapp />
+    </a>
+  )
+}
+
+export default WhatsAppButton
